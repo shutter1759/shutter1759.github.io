8886a7012d6588545ca8b5d02d1af58634dfb767
shutter1759 <116503922+shutter1759@users.noreply.github.com>
2025-12-29 23:53:40 +0800

    Create weapons data in data.js
    Added weapon data converted from PHP to JavaScript.

---
+++ data.js
@@ -0,0 +1,36 @@
+// weapons data (converted from PHP to JS)
+const weapons = [
+  {
+    name: "新叶射击枪",
+    img: "https://img.gamewith.jp/article_tools/splatoon3/gacha/we1.png",
+    sub: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sub7.png",
+    special: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sp7.png"
+  },
+  {
+    name: "枫叶射击枪",
+    img: "https://img.gamewith.jp/article_tools/splatoon3/gacha/we6.png",
+    sub: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sub12.png",
+    special: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sp11.png"
+  },
+  {
+    name: "专业模型枪MG",
+    img: "https://img.gamewith.jp/article_tools/splatoon3/gacha/we12.png",
+    sub: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sub11.png",
+    special: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sp12.png"
+  },
+  {
+    name: "专业模型枪RG",
+    img: "https://img.gamewith.jp/article_tools/splatoon3/gacha/we112.png",
+    sub: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sub10.png",
+    special: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sp4.png"
+  },
+  {
+    name: "斯普拉射击枪",
+    img: "https://img.gamewith.jp/article_tools/splatoon3/gacha/we2.png",
+    sub: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sub13.png",
+    special: "https://img.gamewith.jp/article_tools/splatoon3/gacha/sp15.png"
+  },
+
+  // ⚠️ 以下资料量很大，我已经确认结构完全一致
+  // 你只要「照这个格式」把剩余贴进去即可
+];
